192082f4396b0375fbb6c2dfb44e6828636aa24e
Lysette Swan <lysetteg1023@gmail.com>
2022-11-11 16:20:43 -0700

    All test are passing

--- src/index.js
+++ src/index.js
@@ -1,89 +1,95 @@
-import vorpal from 'vorpal'
-import { prompt } from 'inquirer'
+import vorpal from "vorpal";
+import { prompt } from "inquirer";
 
 import {
   readFile,
   writeFile,
   chooseRandom,
   createPrompt,
-  createQuestions
-} from './lib'
+  createQuestions,
+} from "./lib";
 
-const cli = vorpal()
+const cli = vorpal();
 
 const askForQuestions = [
   {
-    type: 'input',
-    name: 'numQuestions',
-    message: 'How many questions do you want in your quiz?',
-    validate: input => {
-      const pass = input.match(/^[1-9]{1}$|^[1-9]{1}[0-9]{1}$|^100$/)
-      return pass ? true : 'Please enter a valid number!'
-    }
+    type: "input",
+    name: "numQuestions",
+    message: "How many questions do you want in your quiz?",
+    validate: (input) => {
+      const pass = input.match(/^[1-9]{1}$|^[1-9]{1}[0-9]{1}$|^100$/);
+      return pass ? true : "Please enter a valid number!";
+    },
   },
   {
-    type: 'input',
-    name: 'numChoices',
-    message: 'How many choices should each question have?',
-    validate: input => {
-      const pass = input.match(/^(?:[2-4]|0[2-4]|4)$/)
-      return pass ? true : 'Please enter a valid number!'
-    }
-  }
-]
+    type: "input",
+    name: "numChoices",
+    message: "How many choices should each question have?",
+    validate: (input) => {
+      const pass = input.match(/^(?:[2-4]|0[2-4]|4)$/);
+      return pass ? true : "Please enter a valid number!";
+    },
+  },
+];
 
-const createQuiz = title =>
+const createQuiz = (title) =>
   prompt(askForQuestions)
-    .then(answer => createPrompt(answer))
-    .then(promptArray => prompt(promptArray))
-    .then(answer => createQuestions(answer))
-    .then(quiz => writeFile(title, JSON.stringify(quiz)))
-    .then(() => console.log('Quiz created successfully.'))
-    .catch(err => console.log('Error creating the quiz.', err))
+    .then((answer) => createPrompt(answer))
+    .then((promptArray) => prompt(promptArray))
+    .then((answer) => createQuestions(answer))
+    .then((quiz) => writeFile(title, JSON.stringify(quiz)))
+    .then(() => console.log("Quiz created successfully."))
+    .catch((err) => console.log("Error creating the quiz.", err));
 
-const takeQuiz = (title, output) => 
+const takeQuiz = (title, output) =>
   readFile(title)
-    .then(quizData => JSON.parse(quizData))
-    .then(quiz => prompt(quiz))
-    .then(answers => writeFile(output, JSON.stringify(answers)))
-    .catch(err => console.log('Error taking the quiz', err))
+    .then((quizData) => JSON.parse(quizData))
+    .then((quiz) => prompt(quiz))
+    .then((answers) => writeFile(output, JSON.stringify(answers)))
+    .catch((err) => console.log("Error taking the quiz", err));
 
 const takeRandomQuiz = (quizzes, output, n) =>
-  Promise.all(quizzes.map(quizName => readFile(quizName)))
-    .then(quizDataArray => quizDataArray.flatMap(quizData => JSON.parse(quizData)))
-    .then(questions => chooseRandom(questions, n))
-    .then(randomQuestions => prompt(randomQuestions))
-    .then(answer => writeFile(output, JSON.stringify(answer)))
-    .then(() => console.log('Random quiz answers saved successfully.'))
-    .catch(err => console.log('Error creating the random quiz.', err))
+  Promise.all(quizzes.map((quizName) => readFile(quizName)))
+    .then((quizDataArray) =>
+      quizDataArray.flatMap((quizData) => JSON.parse(quizData))
+    )
+    .then((questions) => chooseRandom(questions, n))
+    .then((randomQuestions) => prompt(randomQuestions))
+    .then((answer) => writeFile(output, JSON.stringify(answer)))
+    .then(() => console.log("Random quiz answers saved successfully."))
+    .catch((err) => console.log("Error creating the random quiz.", err));
 
 cli
   .command(
-    'create <fileName>',
-    'Creates a new quiz and saves it to the given fileName'
+    "create <fileName>",
+    "Creates a new quiz and saves it to the given fileName"
   )
   .action(function (input, callback) {
-    return createQuiz(input.fileName)
-  })
+    return createQuiz(input.fileName);
+  });
 
 cli
   .command(
-    'take <fileName> <outputFile>',
-    'Loads a quiz and saves the users answers to the given outputFile'
+    "take <fileName> <outputFile>",
+    "Loads a quiz and saves the users answers to the given outputFile"
   )
   .action(function (input, callback) {
-    return takeQuiz(input.fileName, input.outputFile)
-  })
+    return takeQuiz(input.fileName, input.outputFile);
+  });
 
 cli
   .command(
-    'random <outputFile> <fileNames...>',
-    'Loads a quiz or' +
-      ' multiple quizes and selects a random number of questions from each quiz.' +
-      ' Then, saves the users answers to the given outputFile'
+    "random <outputFile> <fileNames...>",
+    "Loads a quiz or" +
+      " multiple quizes and selects a random number of questions from each quiz." +
+      " Then, saves the users answers to the given outputFile"
   )
   .action(function (input, callback) {
-    return takeRandomQuiz(input.fileNames, input.outputFile, Math.floor(Math.random() * 5))
-  })
+    return takeRandomQuiz(
+      input.fileNames,
+      input.outputFile,
+      Math.floor(Math.random() * 5)
+    );
+  });
 
-cli.delimiter(cli.chalk['yellow']('quizler>')).show()
+cli.delimiter(cli.chalk["yellow"]("quizler>")).show();
--- src/lib.js
+++ src/lib.js
@@ -1,25 +1,94 @@
-import fs from 'fs'
+import fs from "fs";
+import { result } from "underscore";
+import { getSystemErrorName } from "util";
 
-export const chooseRandom = () => {
-  // TODO implement chooseRandom
-}
+export const chooseRandom = (array = [], numItems) => {
+  if (array.length <= 1) {
+    return array;
+  }
+  if (numItems < 1 || numItems > array.length) {
+    numItems = math.floor(math.random() * (array.length + 1));
+  }
 
-export const createPrompt = () => {
-  // TODO implement createPrompt
-}
+  let result = [];
+  let randomlySelectedItemsArray = [...array];
 
-export const createQuestions = () => {
-  // TODO implement createQuestions
-}
+  for (let i = randomlySelectedItemsArray.length - 1; i > 0; i--) {
+    let randomNumber = Math.floor(Math.random() * i);
+    let x = randomlySelectedItemsArray[i];
+    randomlySelectedItemsArray[i] = randomlySelectedItemsArray[randomNumber];
+    randomlySelectedItemsArray[randomNumber] = x;
+  }
 
-export const readFile = path =>
+  return randomlySelectedItemsArray.slice(0, numItems);
+};
+
+export const createPrompt = ({ numQuestions = 1, numChoices = 2 } = {}) => {
+  let result = [];
+
+  for (let question = 1; question <= numQuestions; question++) {
+    result.push({
+      type: "input",
+      name: `question-${question}`,
+      message: `Enter question ${question}`,
+    });
+
+    for (let choice = 1; choice <= numChoices; choice++) {
+      result.push({
+        type: "input",
+        name: `question-${question}-choice-${choice}`,
+        message: `Enter answer choice ${choice} for question ${question}`,
+      });
+    }
+  }
+
+  return result;
+};
+
+export const createQuestions = (obj = {}) => {
+  let questionArray = [];
+  let currentQuestionNumber;
+  let questionName;
+  let message;
+  let choices = [];
+
+  for (let key in obj) {
+    if (!key.includes("choice") || !key.includes(currentQuestionNumber)) {
+      currentQuestionNumber = key;
+      questionName = key;
+      message = obj[key];
+    }
+
+    if (key.includes("choice") && key.includes(currentQuestionNumber)) {
+      choices.push(obj[key]);
+    }
+
+    let keys = Object.keys(obj);
+    let nextIndex = keys.indexOf(key) + 1;
+    let nextItem = keys[nextIndex];
+
+    if (nextItem === undefined || !nextItem.includes(currentQuestionNumber)) {
+      questionArray.push({
+        type: "list",
+        name: questionName,
+        message: message,
+        choices: choices,
+      });
+      choices = [];
+    }
+  }
+  console.log(questionArray);
+  return questionArray;
+};
+
+export const readFile = (path) =>
   new Promise((resolve, reject) => {
-    fs.readFile(path, (err, data) => (err ? reject(err) : resolve(data)))
-  })
+    fs.readFile(path, (err, data) => (err ? reject(err) : resolve(data)));
+  });
 
 export const writeFile = (path, data) =>
   new Promise((resolve, reject) => {
-    fs.writeFile(path, data, err =>
-      err ? reject(err) : resolve('File saved successfully')
-    )
-  })
+    fs.writeFile(path, data, (err) =>
+      err ? reject(err) : resolve("File saved successfully")
+    );
+  });
